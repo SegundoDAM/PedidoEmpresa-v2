dddab88a8a3cd05df86502b767b6eed6bc533fb2
programacion <72938653+ionicharnina2021@users.noreply.github.com>
2023-09-25 11:25:35 +0200

    inicial

---
+++ src/model/data/Pedido.java
@@ -0,0 +1,14 @@
+package model.data;
+
+public abstract class Pedido {
+	//propiedades
+	private int numeroDeSerie;
+	private String fecha;
+	private Persona cliente;
+	private float cuantiaTotal;
+	
+	//Setters and Getters
+
+	//metodos
+	
+}
