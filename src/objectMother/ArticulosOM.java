432181f9fe4d93d2e73089ba7f262d0520850249
programacion <72938653+ionicharnina2021@users.noreply.github.com>
2023-09-25 12:08:05 +0200

    lista mutables vs inmutable

--- src/objectMother/ArticulosOM.java
+++ src/objectMother/ArticulosOM.java
@@ -1,5 +1,7 @@
 package objectMother;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import model.data.Articulo;
@@ -7,16 +9,27 @@ import model.data.Articulo;
 public class ArticulosOM {
 
 	public List<Articulo> getArticulos(){
-		return List.of(
-				new Articulo(1, "Tornillo", 23f),
-				new Articulo(1, "Tornillo", 23f),
-				new Articulo(1, "Tornillo", 23f),
-				new Articulo(1, "Tornillo", 23f),
-				new Articulo(1, "Tornillo", 23f),
-				new Articulo(1, "Tornillo", 23f),
-				new Articulo(1, "Tornillo", 23f),
-				new Articulo(1, "Tornillo", 23f)
-				);
+		//Esta es una lista mutable
+		int[] ids= {1,2,3,4,5};
+		String[] nombres= {"Tornillo","Tornillo","Tornillo","Tornillo","Tornillo"};
+		float[] precios= {23f,23f,23f,23f,23f};
+		LinkedList <Articulo> articulos=new LinkedList<>();
+		for (int i = 0; i < precios.length; i++) {
+			articulos.add(new Articulo(ids[i], nombres[i], precios[i]));
+		} 
+		return articulos;
+		
+		//esta es inumutable
+//		return List.of(
+//				new Articulo(1, "Tornillo", 23f),
+//				new Articulo(1, "Tornillo", 23f),
+//				new Articulo(1, "Tornillo", 23f),
+//				new Articulo(1, "Tornillo", 23f),
+//				new Articulo(1, "Tornillo", 23f),
+//				new Articulo(1, "Tornillo", 23f),
+//				new Articulo(1, "Tornillo", 23f),
+//				new Articulo(1, "Tornillo", 23f)
+//				);
 		
 	}
 }
