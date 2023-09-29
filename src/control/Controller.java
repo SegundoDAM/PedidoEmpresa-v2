0297e72c0c8f1d20272d824b9161125c73afe10f
programacion <72938653+ionicharnina2021@users.noreply.github.com>
2023-09-29 09:32:06 +0200

    v5 primeras peticiones al controller

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -1,14 +1,18 @@
 package control;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
+import model.data.Articulo;
 import model.data.Cliente;
+import model.repositories.ArticuloRepositorio;
 import model.repositories.ClienteRepositorio;
 
 public class Controller {
 	//TODO
 	private ClienteRepositorio clienteRepositorio;
+	private ArticuloRepositorio articuloRepositorio;
 	/**
 	 * Necesito la lista completa de clientes
 	 */
@@ -25,7 +29,22 @@ public class Controller {
 	/**
 	 * Todos los articulos 
 	 */
+	public List<Articulo> getArticulos(){
+		return articuloRepositorio.findAll();
+	}
 	/**
 	 * Todos los articulos en un rango de precios
+	 */
+	public List<Articulo> getArticulosBetweenPrecios(float minimo,float maximo){
+		//clausula guarda
+		if(minimo<=maximo)
+			return articuloRepositorio.findByRange(minimo,maximo);
+		return new LinkedList<Articulo>();
+	}
+	/**
+	 * Los dependientes, la lista de todos
+	 */
+	/**
+	 * Los vendedores con una ruta sur o central
 	 */
 }
--- src/model/repositories/ArticuloRepositorio.java
+++ src/model/repositories/ArticuloRepositorio.java
@@ -2,6 +2,7 @@ package model.repositories;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import model.data.Articulo;
 import objectMother.ArticulosOM;
@@ -26,5 +27,12 @@ public class ArticuloRepositorio {
 				return Optional.of(articulo);
 		}
 		return Optional.ofNullable(null);
+	}
+
+	public List<Articulo> findByRange(float minimo, float maximo) {
+		return articulos.stream()
+				.filter((articulo)->{return articulo.getPrecio()>=minimo;})
+				.filter((articulo)->{return articulo.getPrecio()<=maximo;})
+				.collect(Collectors.toList());
 	}
 }
